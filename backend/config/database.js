7c0752b17463c29ce8fa431b9797edd3f086bf1d
aniket777984 <aniket777984@gmail.com>
2024-05-13 00:08:22 +0530

    database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,11 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose =   require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+const connectDatabase = ()=>{
+    mongoose.connect(process.env.DB_URI,{useNewUrlParser:true , useUnifiedTopology:true}).then((data)=>{
+        console.log(`mongodo connected with server ${data.connection.host}`)
+    }).catch((err)=>{
+        console.log(err);
     })
-    .then((data) => {
-      console.log(`mongodo connected with server ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+}
 
 module.exports = connectDatabase;
